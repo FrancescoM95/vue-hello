dec14d59e4a0be4021af7bd34ec42b006e9d4368
FrancescoM95 <mon95fran@gmail.com>
2024-01-19 14:59:12 +0100

    added vue js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('JS OK');
+console.log('Vue OK', Vue);
+
+const { createApp } = Vue;
+const app = createApp({
+    data() {
+        return {
+            message: 'Ciao! A me piace Vue JS!',
+            image: 'https://www.nuovomille.it/wp-content/uploads/2023/07/origine-di-ciao-768x512.jpg'
+        }
+    }
+});
+app.mount('#root');
